89a80525d61ec159aa1590bb1bb1f6d8490d1612
atarannn <anastasiatarannn@icloud.com>
2021-04-03 15:18:20 +0300

    added header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,27 @@
 import React from "react";
 import NavBar from "./NavBar";
+import {Col, Container, Nav, Navbar, NavLink, Row} from "react-bootstrap";
+import {HOME_ROUTE} from "../utils/consts";
+import logo from "../img/logo.jpg";
 
 function Header() {
     return (
-        <NavBar />
-    );
+        <Navbar bg="white" className="header-nav-inner" fixed="top">
+            <Container>
+                <Row className="w-100 justify-content-between align-items-center">
+                    <Col>
+                        <NavLink href={HOME_ROUTE}>
+                            <img src={logo}></img>
+                         </NavLink>
+                    </Col>
+                     <Col lg={8}>
+                        <NavBar />
+                    </Col>
+                </Row>
+            </Container>
+        </Navbar>
+
+);
 }
 
 export default Header;
