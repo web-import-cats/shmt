84a4c290e8dc25baffdef30482b5d2cebeef7ea0
atarannn <anastasiatarannn@icloud.com>
2021-04-03 13:22:38 +0300

    home page layout

--- src/components/Main.js
+++ src/components/Main.js
@@ -5,10 +5,12 @@ import logo from "../img/logo.jpg";
 function Main() {
     return (
         <div id="main-page">
-
             <Container>
-                <div className="w-100 d-flex flex-wrap justify-content-between align-items-center">
+                <div className="w-100 h-100 d-flex flex-wrap justify-content-around align-items-center">
+                    <img src={logo}></img>
                     <div className="d-flex flex-column justify-content-center align-items-end">
+                        <img className="main-page-window" src={logo}></img>
+                        <button className="main-page-button">Просмотр шмоток</button>
                     </div>
                 </div>
             </Container>
