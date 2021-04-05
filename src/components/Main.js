f6334b1a090edb7c11832d826862cb628d3f1221
atarannn <anastasiatarannn@icloud.com>
2021-04-05 16:16:28 +0300

    finished main page

--- src/components/Main.js
+++ src/components/Main.js
@@ -7,12 +7,16 @@ function Main() {
     return (
         <div id="main-page">
             <Container>
-                <div className="w-100 h-100 d-flex flex-column flex-md-row justify-content-md-around justify-content-start align-items-center">
-                    <img src={Image} className="main-page-Image"></img>
-                    <div className="main-page-right">
-                        <div className="d-flex flex-column justify-content-center align-items-md-end align-items-center ">
-                            <img src={Window} className="main-page-window"></img>
-                            <button className="main-page-button">Просмотр шмоток</button>
+                <div className="h-100 d-flex justify-content-center  align-items-md-center align-items-start">
+                    <div className="main-page-wrap">
+                        <div className="d-flex flex-column flex-md-row justify-content-md-around justify-content-start align-items-center">
+                            <img src={Image} className="main-page-Image"></img>
+                            <div className="main-page-right">
+                                <div className="d-flex flex-column justify-content-center align-items-md-end align-items-center ">
+                                    <img src={Window} className="main-page-window"></img>
+                                    <button className="main-page-button">Просмотр шмоток</button>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
