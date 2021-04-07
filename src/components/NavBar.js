158db1e36536e1cc6f20a93af92f3fde0d33c143
atarannn <anastasiatarannn@icloud.com>
2021-04-08 00:49:58 +0300

    font

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -8,6 +8,7 @@ let history = createBrowserHistory();
 const NavBar = () => {
     const {user} = useContext(Context)
     return (
+        <div class="nav-bar-wrap">
         <div className="nav-bar">
             <Nav className="justify-content-around align-items-center">
                 <Nav.Item as="li" className="header-nav-item"><Nav.Link className="header-nav-link" href="shmot">Шмотки</Nav.Link></Nav.Item>
@@ -15,6 +16,19 @@ const NavBar = () => {
                 <Nav.Item as="li" className="header-nav-item"><Nav.Link className="header-nav-link" href="about">Про нас</Nav.Link></Nav.Item>
                 <Nav.Item as="li" className="header-nav-item"><Nav.Link className="header-nav-link login-link" href="login">Войти</Nav.Link></Nav.Item>
             </Nav>
+        </div>
+        <div class="hamburger-menu">
+            <input id="menu__toggle" type="checkbox" />
+            <label class="menu__btn" for="menu__toggle"><span></span></label>
+
+            <Nav as="ul" class="menu__box">
+                <Nav.Item as="li"><Nav.Link class="menu__item" href="#">Главная</Nav.Link></Nav.Item>
+                <Nav.Item as="li"><Nav.Link class="menu__item" href="#">Проекты</Nav.Link></Nav.Item>
+                <Nav.Item as="li"><Nav.Link class="menu__item" href="#">Команда</Nav.Link></Nav.Item>
+                <Nav.Item as="li"><Nav.Link class="menu__item" href="#">Блог</Nav.Link></Nav.Item>
+                <Nav.Item as="li"><Nav.Link class="menu__item" href="#">Контакты</Nav.Link></Nav.Item>
+            </Nav>
+        </div>
         </div>
 );
 };
