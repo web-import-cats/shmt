49295cc200667769b1a2a7c9ba875390a5a6b3ac
atarannn <anastasiatarannn@icloud.com>
2021-04-08 19:05:01 +0300

    login page layout

--- src/pages/Auth.js
+++ src/pages/Auth.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import {Container} from "react-bootstrap";
 
 const Auth = () => {
     return (
-        <div>
-            Auth
+        <div id = "login-page">
+            <Container>
+                <div class = "h-100 d-flex flex-column justify-content-center align-items-center">
+                    <h2 className="auth-caption">Вход в аккаунт</h2>
+                    <form className="auth-form-wrap">
+                        <div className="d-flex flex-column justify-content-center align-items-center">
+                            <input type="email" placeholder = "Email" className="auth-form-input"></input>
+                            <input type="password" placeholder = "Пароль" className="auth-form-input"></input>
+                            <button className="auth-form-button">Продолжить</button>
+                        </div>
+                    </form>
+                </div>
+            </Container>
         </div>
     );
 };
